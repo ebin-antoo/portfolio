c4f586fde48ed7c9d9e57c24f7124b0671d89822
ebin-antoo <ebin@ebin-antoo.com>
2014-09-23 14:22:31 -0400

    Working Parallex & added animate.css effects

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -97,3 +97,32 @@ if (!$("html").hasClass("touch")) {
     $(window).scroll(parallaxPosition);
     parallaxPosition();
 }
+
+/******************************************************************************************/
+/* affix the navbar after scroll below main */
+$('#nav').affix({
+    offset: {
+        top: $('.main').height()-$('#nav').height()
+    }
+});
+
+
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' })
+
+/* smooth scrolling for scroll to top */
+$('.scroll-top').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+})
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top;
+  $('body,html').animate({scrollTop:posi},700);
+});
+
+
+
+
+
